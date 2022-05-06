efc5e8619b895418ff9f80c2ee041e8ed9a3d3e5
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-05-06 19:05:17 +0100

    adding mapbox to the map.js

--- pages/map.js
+++ pages/map.js
@@ -2,25 +2,25 @@ import Dashboard from "../components/Dashboard"
 
 export default function Map() {
     return (
-        <Dashboard>
-            <main className="rounded overflow-hidden shadow-lg border border-gray-200 bg-white">
-                <div className="sm:text-center lg:text-left">
-                    <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                        <span className="block xl:inline">Map</span>{' '}
-                        <span className="block text-[#C416EC] xl:inline">Map</span>
-                    </h1>
-                    <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                        Kanda data has been collected from local stations which are equiped with weather sensors connected to the Internet. All the data are store into the Telos Blockchain.
-                        The blue markers are the Telos node of the blockchain and the gray markers are the sensors.
-                        
-                    </p>
-                    <div className="mt-10 sm:mt-16 sm:flex sm:justify-center lg:justify-start">
-                        <div className="rounded-md shadow">
+        <div className="justify-center">
+            <Dashboard>
+                <h1 className="mt-10 text-center text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                    <span className="block text-[#C416EC] xl:inline">Kanda Weather Stations</span>{' '}
+                    <span className="block xl:inline">in the world.</span>
+                </h1>
 
-                        </div>
+                
+            </Dashboard>
+            <main className="w-11/12 mt-10 ml-auto mr-auto rounded  shadow-lg border border-gray-200 bg-white">
+                <div className="sm:text-center lg:text-center">
+
+
+                    <div className="sm:mt-16 sm:flex sm:justify-center lg:justify-start">
+                            
                     </div>
                 </div>
             </main>
-        </Dashboard>
+        </div>
+        
     )
 }
