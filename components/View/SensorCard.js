bf7a5e8dbcb90854b8048922662cfb5cf9f5f1a3
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-05 13:21:45 +0100

    Added 2 fields in the sensor Card

--- components/View/SensorCard.js
+++ components/View/SensorCard.js
@@ -8,7 +8,7 @@ export default function SensorCard(props) {
       <div className='rounded overflow-hidden shadow-lg border border-gray-200 bg-white'>
         <div className=''>
             <div className="p-12 ml-5">
-                <h5 className="mt-5 mb-4 text-xl font-medium text-[#8C01AA]"> Sensor Information</h5>
+                <h5 className="mt-1 mb-4 text-xl font-medium text-[#8C01AA]"> Sensor Information</h5>
                 <div className="flex items-baseline text-gray-900">
                     <span className="ml-5 text-xl font-normal text-gray-500 ">Devname:</span>
                     <span className="ml-2 text-3xl font-semibold">
@@ -53,6 +53,14 @@ export default function SensorCard(props) {
                     <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-red-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                         <span className="text-base font-normal leading-tight text-gray-500">Longitude: {props.sensor.lo}</span>
+                    </li>
+                    <li className="flex space-x-3">
+                        <svg className="flex-shrink-0 w-5 h-5 text-[#130f40]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                        <span className="text-base font-normal leading-tight text-gray-500">Lastest temperaure: {props.sensor.lo}</span>
+                    </li>
+                    <li className="flex space-x-3">
+                        <svg className="flex-shrink-0 w-5 h-5 text-[#f9ca24]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
+                        <span className="text-base font-normal leading-tight text-gray-500">Last uploaded: {props.sensor.lo} ago</span>
                     </li>
                     <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-gray-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
