4fe8acf4d0608870db6584cf02b741db25599e2f
Bayangmbe Mounmo <bayangp0@gmail.com>
2022-08-06 12:48:58 +0100

    changed last uploaded to last updated

--- components/View/SensorCard.js
+++ components/View/SensorCard.js
@@ -69,7 +69,7 @@ export default function SensorCard(props) {
                     </li>
                     <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-[#f9ca24]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                        <span className="text-base font-normal leading-tight text-gray-500">Last uploaded: {props.sensor.last_update}</span>
+                        <span className="text-base font-semibold leading-tight text-gray-500">Last updated: {props.sensor.last_update}</span>
                     </li>
                 </ul>
                 {/* <button type="button" className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-200 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center">Choose plan</button> */}
