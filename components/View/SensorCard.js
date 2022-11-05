8caca96854985f13abc6ee225d024343ea5aa527
sunburntcat <32531379+sunburntcat@users.noreply.github.com>
2022-11-05 15:18:18 -0500

    Remove miner field

--- components/View/SensorCard.js
+++ components/View/SensorCard.js
@@ -53,15 +53,6 @@ export default function SensorCard(props) {
                         <span className="text-base font-normal leading-tight text-gray-500">Longitude: {props.sensor.lo}</span>
                     </li>
                     <li className="flex space-x-3">
-                        <svg className="flex-shrink-0 w-5 h-5 text-gray-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                        <span className="flex gap-2 text-base font-normal leading-tight text-gray-500 ">
-                            Miner: 
-                            <span className="text-base font-semibold leading-tight text-gray-500 ">
-                                {props.sensor.miner}
-                            </span>
-                        </span>
-                    </li>
-                    <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-[#130f40]" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                         <span className="text-base font-normal leading-tight text-gray-500">Latest temperaure: {props.sensor.last_temp} °C</span>
                     </li>
