b7e53102e62e98c4fbd39e5bdf1fbc040f5ecd81
Nicolas Lopez <32531379+sunburntcat@users.noreply.github.com>
2023-01-13 14:17:40 -0600

    update margins

--- components/View/SensorCard.js
+++ components/View/SensorCard.js
@@ -23,14 +23,14 @@ export default function SensorCard(props) {
             <div className="p-12 ml-1">
                 <h5 className="mt-1 mb-4 text-xl font-medium text-[#8C01AA]"> Sensor Information</h5>
                 <div className="flex items-baseline text-gray-900">
-                    <span className="ml-5 text-xl font-normal text-gray-500 ">Devname:</span>
+                    <span className="ml-0 text-xl font-normal text-gray-500 ">Devname:</span>
                     <span className="ml-2 text-3xl font-semibold">
                         {
                             props.sensor.devname
                         }
                     </span>
                 </div>
-                <ul role="list" className="my-7 space-y-2 ml-5">
+                <ul role="list" className="my-7 space-y-2 ml-0">
                     <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-purple-400 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
                         {
