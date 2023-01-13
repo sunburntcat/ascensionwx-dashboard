bfec7742745e4000337346763591a3161afe8e37
Nicolas Lopez <32531379+sunburntcat@users.noreply.github.com>
2023-01-13 14:34:14 -0600

    Update SensorCard.js

--- components/View/SensorCard.js
+++ components/View/SensorCard.js
@@ -20,7 +20,7 @@ export default function SensorCard(props) {
     return (
       <div className='rounded overflow-hidden shadow-lg border border-gray-200 bg-white'>
         <div className=''>
-            <div className="p-8 ml-1">
+            <div className="p-8 ml-0">
                 <h5 className="mt-1 mb-4 text-xl font-medium text-[#8C01AA]"> Sensor Information</h5>
                 <div className="flex items-baseline text-gray-700">
                     <span className="ml-0 text-xl font-normal text-gray-500 ">Devname:</span>
@@ -57,11 +57,11 @@ export default function SensorCard(props) {
                     </li>
                     <li className="flex space-x-3">
                         <svg className="flex-shrink-0 w-5 h-5 text-green-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd"></path></svg>
-                        <span className="text-base font-normal leading-tight text-gray-400">Location: {fullLocation}</span>
+                        <span className="text-base font-normal leading-tight text-gray-500">Location: {fullLocation}</span>
                     </li>
                     <li className="flex space-x-3">
                         <img src="thermometer.png" width="20" height="20"/>
-                        <span className="text-base font-normal leading-tight text-gray-500">Latest Temperature: {temperature}°C/{temperature_f}°F</span>
+                        <span className="text-base font-normal leading-tight text-gray-500">Latest Temperature: {temperature}°C / {temperature_f}°F</span>
                     </li>
                     <li className="flex space-x-3">
                         <img src="humidity.jpg" width="20" height="20"/>
